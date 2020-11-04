a6bdafd7d333b64908e848f208adb266086e7b5b
Zach Bayard <zbayard@gmail.com>
2020-11-04 15:30:32 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -135,3 +135,11 @@ def num_points_scored(player_name)
     end
   end
 end
+
+def shoe_size(player_name)
+  game_hash.each do |home_away, keys|
+    keys[:players].each do |player|
+      return player[:shoe] if player[:player_name] == player_name
+    end
+  end
+end
