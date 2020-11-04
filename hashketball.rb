def484f1f0f93a98ce29cd7e8e9e4871e88856fe
Zach Bayard <zbayard@gmail.com>
2020-11-04 15:18:51 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -129,6 +129,10 @@ end
 
 # Write code here
 def num_points_scored(player_name)
-  players = game_hash[:home][:players] << (game_hash[:away][:players])
-  players[player_name][:points]
+game_hash.each do |team, team_data|
+  team_data[:players].each do |player|
+    if player[:player_name] == player_name
+      return player[:points]
+    end
+  end
 end
