5d887e2cc2f6bb25461575d44a006abe92ece46e
Zach Bayard <zbayard@gmail.com>
2020-11-04 15:39:48 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -173,3 +173,18 @@ def player_stats(player_name)
     end
   end
 end
+
+def big_shoe_rebounds
+  biggest = 0
+  rebounds = 0
+  game_hash.each do |home_away, keys|
+    keys[:players].each do |player|
+      size = player[:shoe]
+      if size > biggest
+        biggest = size
+        rebounds = player[:rebounds]
+      end
+    end
+  end
+  rebounds
+end
